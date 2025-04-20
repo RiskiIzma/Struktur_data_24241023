ee957d5a74467d9461209a806a03e7e02edff298
RiskiIzma <riskiismaperdani75@gmail.com>
2025-04-20 22:28:24 +0800

    Update TUGAS3.py

--- PRAKTIKUM/PRE-1 PERULANGAN/TUGAS3.py
+++ PRAKTIKUM/PRE-1 PERULANGAN/TUGAS3.py
@@ -2,7 +2,7 @@ n = int(input("Masukkan jumlah deret Fibonacci: "))
 
 a, b = 0, 1 # a akan menyimpan angka sekarang , b akan menyimpan angka berikutnya
 
-print("Deret Fibonacci:", end=',')
+print("Deret Fibonacci:", end=' ')
 for i in range(n):
     print(a, end=',')  # Cetak angka Fibonacci ke samping
     a, b = b, a + b     # ini adalah rumus dari fibonacci
