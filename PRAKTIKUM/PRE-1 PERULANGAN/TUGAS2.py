7911707a43e72315de53451538881491845a1cdd
RiskiIzma <riskiismaperdani75@gmail.com>
2025-04-20 21:58:49 +0800

    Update TUGAS2.py

--- PRAKTIKUM/PRE-1 PERULANGAN/TUGAS2.py
+++ PRAKTIKUM/PRE-1 PERULANGAN/TUGAS2.py
@@ -4,4 +4,4 @@ angka_range = range(1, jumlah + 1)  # Misalnya: jika jumlah = 5, maka range(1, 6
 
 print("Bilangan genap yang ditampilkan:", end=' ')
 for i in angka_range:
-    print(i * 2, end=',')  # Cetak ke samping, dipisahkan oleh spasi
+    print(i * 2, end=',')  # Cetak ke samping, dipisahkan oleh koma
