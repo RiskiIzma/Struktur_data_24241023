4b0ebab62b3b0b33a1c1d5ddf67a739ab7b2dd37
RiskiIzma <riskiismaperdani75@gmail.com>
2025-04-28 10:45:56 +0800

    Update TugasFunction.py

--- PRAKTIKUM/PRE-1 PERULANGAN/TugasFunction.py
+++ PRAKTIKUM/PRE-1 PERULANGAN/TugasFunction.py
@@ -1,30 +1,30 @@
-# variabel gloal untuk menyimpan data siswa
-siswa_list = []
+# variabel gloal untuk menyimpan data mahasiswa
+mahasiswa_list = []
 
 def create():
-    """Menampilkan nama siswa ke dalam list"""
-    nama = input("Menambahkan nama siswa: ")
-    siswa_list.append(nama)
+    """Menampilkan nama mahasiswa ke dalam list"""
+    nama = input("Menambahkan nama mahasiswa: ")
+    mahasiswa_list.append(nama)
     print(f"nama '{nama}' telah ditambahkan")
 
 def read():
-    """Menampilkan data siswa"""
-    if not siswa_list:
-        print("Tidak ada data siswa")
+    """Menampilkan data mahasiswa"""
+    if not mahasiswa_list:
+        print("Tidak ada data mahasiswa")
     else:
-        print("Daftar siswa")
-        for index , nama in enumerate(siswa_list):
+        print("Daftar Mahasiswa")
+        for index , nama in enumerate(mahasiswa_list):
             print(f"{index + 1}. {nama}")
 
 def update():
-    """Mengubah nama siswa berdasarkan indeks"""
+    """Mengubah nama mahasiswa berdasarkan indeks"""
     read()
     try:
-        indeks = int(input("Masukan nomor siswa yang ingin diubah: ")) -1
-        if 0 <= indeks < len(siswa_list):
+        indeks = int(input("Masukan nomor mahasiswa yang ingin diubah: ")) -1
+        if 0 <= indeks < len(mahasiswa_list):
             nama_baru = input("Masukan nama baru")
-            siswa_list[indeks] = nama_baru
-            print("Nama siswa berhasil diubah.")
+            mahasiswa_list[indeks] = nama_baru
+            print("Nama mahasiswa berhasil diubah.")
         else:
             print("indeks tidak valid")
     except ValueError:
@@ -32,12 +32,12 @@ def update():
 
 
 def delete():
-    """Menghapus nama siswa berdasarkan indeks."""
+    """Menghapus nama mahasiswa berdasarkan indeks."""
     read()
     try:
-        indeks = int(input("Masukkan nomor siswa yang ingin dihapus: ")) - 1
-        if 0 <= indeks < len(siswa_list):
-            nama_dihapus = siswa_list.pop(indeks)
+        indeks = int(input("Masukkan nomor mahasiswa yang ingin dihapus: ")) - 1
+        if 0 <= indeks < len(mahasiswa_list):
+            nama_dihapus = mahasiswa_list.pop(indeks)
             print(f"Nama '{nama_dihapus}' berhasil dihapus.")
         else:
             print("Indeks tidak valid.")
@@ -47,10 +47,10 @@ def delete():
 def main():
     while True:
         print("\nMenu:")
-        print("1. Tambah Siswa")
-        print("2. Tampilkan Siswa")
-        print("3. Ubah Siswa")
-        print("4. Hapus Siswa")
+        print("1. Tambah mahasiswa")
+        print("2. Tampilkan Mahasiswa")
+        print("3. Ubah Mahasiswa")
+        print("4. Hapus Mahasiswa")
         print("5. Keluar")
         
         pilihan = input("Pilih aksi (1-5): ")
